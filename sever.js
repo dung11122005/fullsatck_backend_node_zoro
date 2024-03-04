116574b9b1cc9383d9f54190600ae20b0e9bdc95
dung11122005 <145743207+dung11122005@users.noreply.github.com>
2024-03-04 22:29:53 +0700

    thaydoi 1

--- sever.js
+++ sever.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World from javascrip\n');
+    res.end('Hello World from javascrip, \n HOANG TAN DUNG');
 });
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
