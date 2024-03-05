c2321361c28800adc0c45a4729f3da33e84b33ef
dung11122005 <145743207+dung11122005@users.noreply.github.com>
2024-03-05 21:47:22 +0700

    thaydoi

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
-const express = require('express')//commonjs
+const express = require('express');//commonjs
+// const { hostname } = require('os');
 const path = require('path');//commonjs
+require('dotenv').config()
+console.log('>>> check env', process.env);
 
 
 const app = express()// app express
-const port = 8080// port
+const port = process.env.PORT || 8888; // port(8080) => hardcode . uat . pord
+const hostname = process.env.HOST_NAME;// hostname(localhost)
 
 
 //config template engine 
@@ -16,15 +20,12 @@ app.get('/', (req, res) => {
     // res.send('Hello World!')
     res.render('sample.ejs');
 })
-app.get('/abcd', (req, res) => {
-    res.send('HOANG TAN DUNG LOVE BACKEND')
-})
-app.get('/jszero', (req, res) => {
+app.get('/abc', (req, res) => {
     res.send('<h1>DO THI QUYNH ANH </h1>')
 })
 
 
 /////////////////////////////////
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
