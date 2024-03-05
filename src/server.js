d3f3e77f3d4666d51d91009241aa6ee3939fe4a6
dung11122005 <145743207+dung11122005@users.noreply.github.com>
2024-03-05 23:16:33 +0700

    thaydoi

--- src/server.js
+++ src/server.js
@@ -15,6 +15,10 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs')
 
 
+//config static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 // khai bao route
 app.get('/', (req, res) => {
     // res.send('Hello World!')
