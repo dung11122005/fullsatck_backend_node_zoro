527a82ce38489a7d4b5fa1bdaba6b9db6c692430
dung11122005 <145743207+dung11122005@users.noreply.github.com>
2024-03-05 07:39:23 +0700

    thaydoi

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')//commonjs
+const path = require('path');//commonjs
+
+
 const app = express()// app express
 const port = 8080// port
 
+
+//config template engine 
+app.set('views', path.join(__dirname, './src/views'));
+app.set('view engine', 'ejs')
+
+
 // khai bao route
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    // res.send('Hello World!')
+    res.render('sample.ejs');
 })
 app.get('/abcd', (req, res) => {
     res.send('HOANG TAN DUNG LOVE BACKEND')
@@ -13,6 +23,8 @@ app.get('/jszero', (req, res) => {
     res.send('<h1>DO THI QUYNH ANH </h1>')
 })
 
+
+/////////////////////////////////
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
